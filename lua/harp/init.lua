a86b84ef8f7b668d0f9616eb8f7a02cec36ec78e
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-04-27 19:42:30 +0800

    integrate split_by_newlines & get_char as parts of the harp library

--- lua/harp/init.lua
+++ lua/harp/init.lua
@@ -6,17 +6,24 @@ local M = {}
 -- Maybe you don't care about some features, or want to make your own; maybe the default mappings don't make sense for you and you want to change them.
 
 -- You'll see this function get used throughout most, if not all, mappings. The reason for it to exist is simply so we don't have to make a billion different mappings per every key.
+
 --- Get a character from the user, unless they press escape, in which case return nil, usually to cancel whatever action the user wanted to do.
-local function get_char(prompt)
+--- @param prompt string What text to show when asking the user for the character
+function M.get_char(prompt)
+	prompt = prompt or ''
 	vim.api.nvim_echo({ { prompt, 'Input' } }, true, {})
 	local char = vim.fn.getcharstr()
-	-- That's the escape character (<Esc>). Not sure how to specify it smarter
+	-- That's the escape character (`<Esc>`). Not sure how to specify it smarter
 	-- In other words, if you pressed escape, we return nil
+	---@diagnostic disable-next-line: cast-local-type
 	if char == '' then char = nil end
 	return char
 end
 
-local function split_by_newlines(string)
+--- Split input by lines, get an array-like table with each line.
+--- @param string string
+---@return table
+function M.split_by_newlines(string)
 	local lines = {}
 	for line in string.gmatch(string, '([^\n]+)') do
 		table.insert(lines, line)
@@ -28,7 +35,7 @@ function M.default_get()
 	-- when you'll press your remap to get (<Leader>s by default), you'll see "get harp: " in your statusline
 	-- this is just a message to let you know the action you're doing
 	-- you can remove it by specifying an empty string instead (''), or defining your own message that makes more sense to you
-	local register = get_char('get harp: ')
+	local register = M.get_char('get harp: ')
 	-- this effectively means that you can press <Escape> to cancel out of this entire function
 	if register == nil then return end
 	local output = vim.fn.system('harp get harps ' .. register .. ' --path')
@@ -47,7 +54,7 @@ function M.default_get()
 end
 
 function M.default_set()
-	local register = get_char('set harp: ')
+	local register = M.get_char('set harp: ')
 	if register == nil then return end
 	-- gets the full path of the current buffer.
 	-- however, if it's in your home directory, /home/username will instead be displayed as ~
@@ -63,7 +70,7 @@ function M.default_set()
 end
 
 function M.percwd_get()
-	local register = get_char('get local harp: ')
+	local register = M.get_char('get local harp: ')
 	if register == nil then return end
 	local cwd = vim.fn.getcwd()
 	-- if `cwd` has /home/username, that will be replaced with ~
@@ -83,7 +90,7 @@ function M.percwd_get()
 end
 
 function M.percwd_set()
-	local register = get_char('set local harp: ')
+	local register = M.get_char('set local harp: ')
 	if register == nil then return end
 	local cwd = vim.fn.getcwd()
 	cwd = vim.fn.fnamemodify(cwd, ':~')
@@ -104,7 +111,7 @@ function M.percwd_set()
 end
 
 function M.cd_get()
-	local register = get_char('get cd harp: ')
+	local register = M.get_char('get cd harp: ')
 	if register == nil then return end
 	-- `harp get 'cd_harps' a --path`
 	-- I'm a fish shell user, but `system` still calls commands in bash (if not sh 🤔) fwiw.
@@ -120,7 +127,7 @@ function M.cd_get()
 end
 
 function M.cd_set()
-	local register = get_char('set cd harp: ')
+	local register = M.get_char('set cd harp: ')
 	if register == nil then return end
 	local cwd = vim.fn.getcwd()
 	cwd = vim.fn.fnamemodify(cwd, ':~')
@@ -130,7 +137,7 @@ function M.cd_set()
 end
 
 function M.perbuffer_mark_get()
-	local register = get_char('get local mark: ')
+	local register = M.get_char('get local mark: ')
 	if register == nil then return end
 	local path = vim.fn.expand('%:~')
 	-- `harp get 'local_marks_~/prog/dotfiles/colors.css' a --line --column`
@@ -141,7 +148,7 @@ function M.perbuffer_mark_get()
 	-- we could instead just call harp twice and not have to split the output by newlines, but that would be slower
 	-- probably not noticeably slower, but technically bad
 	if vim.v.shell_error == 0 and output then
-		local lines = split_by_newlines(output)
+		local lines = M.split_by_newlines(output)
 		local line = lines[1]
 		local column = lines[2]
 		-- whenever you see `vim.fn`, that means that you can search for the documentation for the next word (in this case, `cursor`) like `:help cursor()`
@@ -153,7 +160,7 @@ function M.perbuffer_mark_get()
 end
 
 function M.perbuffer_mark_set()
-	local register = get_char('set local mark: ')
+	local register = M.get_char('set local mark: ')
 	if register == nil then return end
 	local path = vim.fn.expand('%:~')
 	local cursor = vim.api.nvim_win_get_cursor(0)
@@ -165,12 +172,12 @@ function M.perbuffer_mark_set()
 end
 
 function M.global_mark_get()
-	local register = get_char('get global mark: ')
+	local register = M.get_char('get global mark: ')
 	if register == nil then return end
 	-- `harp get 'global_marks' a --path --line --column`
 	local output = vim.fn.system("harp get 'global_marks' " .. register .. ' --path --line --column')
 	if vim.v.shell_error == 0 and output then
-		local lines = split_by_newlines(output)
+		local lines = M.split_by_newlines(output)
 		local path = lines[1]
 		local line = lines[2]
 		local column = lines[3]
@@ -182,7 +189,7 @@ function M.global_mark_get()
 end
 
 function M.global_mark_set()
-	local register = get_char('set global mark: ')
+	local register = M.get_char('set global mark: ')
 	if register == nil then return end
 	local path = vim.fn.expand('%:~')
 	local cursor = vim.api.nvim_win_get_cursor(0)
